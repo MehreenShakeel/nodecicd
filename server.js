cb721a921623aefefd7626bd9a4c496d0fb924f4
MehreenShakeel <mehreensk8@gmail.com>
2023-08-31 16:22:49 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+res.send("hello world!");
+
+ app.listen(port, ()=>{
+  console.log(`server running on port $(port)`);
+ });
